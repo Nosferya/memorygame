e0a83112bfafd858ba585a196cd43810e3baaa51
Inseri <39262186+Inseri@users.noreply.github.com>
2018-06-14 17:03:50 +0200

    Add files via upload

---
+++ test/js/anim.js
@@ -0,0 +1,18 @@
+var divpict = document.getElementById('pict');
+divpict.style.width = "150px";
+divpict.style.height = "150px";
+
+var pictclick = document.getElementById("pict");
+pictclick.addEventListener('click', cha);
+let nb = 0;
+
+function cha() {
+    if (nb == 0) {
+        pict.style.backgroundColor = 'red';
+        nb = 1;
+    } else {
+        pict.style.backgroundColor = 'black';
+        nb = 0;
+    }
+
+};
