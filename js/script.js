775c51beeb92159b090b8d191401067e8f1023c6
Greniert <grenierth@hotmail.fr>
2018-06-15 15:22:56 +0200

    end of the game

--- js/script.js
+++ js/script.js
@@ -60,16 +60,14 @@ gameGrid.forEach(item => { //pour chaque item de la grille de carte = créer une
   card.dataset.name = item.name;
   card.style.backgroundImage = `url(${item.img})`;
 
-  // Create front of card
  const front = document.createElement('div');
  front.classList.add('front');
 
- // Create back of card, which contains
  const back = document.createElement('div');
  back.classList.add('back');
  back.style.backgroundImage = `url(${item.img})`;
 
- // Append card to grid, and front and back to each card
+
  grid.appendChild(card);
  card.appendChild(front);
  card.appendChild(back);
@@ -79,23 +77,20 @@ gameGrid.forEach(item => { //pour chaque item de la grille de carte = créer une
 grid.addEventListener('click', function (event) {
 
   let clicked = event.target;
-  if (clicked.nodeName === 'SECTION' || clicked === previousTarget) { return; }
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected')) { return; }
   if (count < 2) {
     count++;
     if (count === 1) {
-      // Assign first guess
-      firstGuess = clicked.dataset.name;
-      clicked.classList.add('selected');
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
     } else {
-      // Assign second guess
-      secondGuess = clicked.dataset.name;
-      clicked.classList.add('selected');
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
     }
-    // If both guesses are not empty...
     if (firstGuess !== '' && secondGuess !== '') {
-      // and the first guess matches the second match...
       if (firstGuess === secondGuess) {
-        // run the match function
         setTimeout(match, delay);
         setTimeout(resetGuesses, delay);
       }
