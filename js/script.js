333a77924a41d4dd9b45a02d97015414cf1a0d33
Inseri <39262186+Inseri@users.noreply.github.com>
2018-06-15 16:10:06 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,61 +1,61 @@
 const cardsArray = [{ //tableau d'image
-    'name': 'canari',
-    'img': 'img/canari.jpg',
-  },
-  {
-    'name': 'chat',
-    'img': 'img/chat.jpg',
-  },
-  {
-    'name': 'cheval',
-    'img': 'img/cheval.jpg',
-  },
-  {
-    'name': 'chien',
-    'img': 'img/chien.jpg',
-  },
-  {
-    'name': 'cochondDinde',
-    'img': 'img/cochoninde.jpg',
-  },
-  {
-    'name': 'hamster',
-    'img': 'img/hamster.jpg',
-  },
-  {
-    'name': 'lapin',
-    'img': 'img/lapin.jpg',
-  },
-  {
-    'name': 'poissonrouge',
-    'img': 'img/poissonrouge.jpg',
-  },
-  {
-    'name': 'renard',
-    'img': 'img/renard.jpg',
-  },
-  {
-    'name': 'tortue',
-    'img': 'img/tortue.jpg',
-  },
+        'name': 'canari',
+        'img': 'img/canari.jpg',
+    },
+    {
+        'name': 'chat',
+        'img': 'img/chat.jpg',
+    },
+    {
+        'name': 'cheval',
+        'img': 'img/cheval.jpg',
+    },
+    {
+        'name': 'chien',
+        'img': 'img/chien.jpg',
+    },
+    {
+        'name': 'cochondDinde',
+        'img': 'img/cochoninde.jpg',
+    },
+    {
+        'name': 'hamster',
+        'img': 'img/hamster.jpg',
+    },
+    {
+        'name': 'lapin',
+        'img': 'img/lapin.jpg',
+    },
+    {
+        'name': 'poissonrouge',
+        'img': 'img/poissonrouge.jpg',
+    },
+    {
+        'name': 'renard',
+        'img': 'img/renard.jpg',
+    },
+    {
+        'name': 'tortue',
+        'img': 'img/tortue.jpg',
+    },
 ];
 
-let gameGrid = cardsArray.concat(cardsArray); //duplique le tableau d'image
-gameGrid.sort(() => 0.5 - Math.random()); // randommise le jeu (gameGrid) avec la foonction
+/*let gameGrid = cardsArray.concat(cardsArray); //duplique le tableau d'image
+gameGrid.sort(() => 0.5 - Math.random());
 
-let firstGuess = ''; // 1 er choix avec des '' vides pour que l'image soit récupérer
-let secondGuess = '';// Pareille pour 2 eme choix
+let firstGuess = '';
+let secondGuess = '';
 let count = 0;
 let previousTarget = null;
 let delay = 1000;
 
 const jeu = document.getElementById('jeu'); // récupère l'id Jeu
 const grid = document.createElement('section'); // créer une grille
-grid.setAttribute('class', 'grid'); //donne la class Grid à la section
+grid.setAttribute('class', 'grid');
 jeu.appendChild(grid); // met la grille en enfant de la div jeu
 
 gameGrid.forEach(item => { //pour chaque item de la grille de carte = créer une div
-  const card = document.createElement('div'); // la constante
+  const card = document.createElement('div');
   card.classList.add('card');
   card.dataset.name = item.name;
   card.style.backgroundImage = `url(${item.img})`;
@@ -73,23 +73,6 @@ gameGrid.forEach(item => { //pour chaque item de la grille de carte = créer une
  card.appendChild(back);
 });
 
-const match = () => {
- var selected = document.querySelectorAll('.selected');
- selected.forEach(card => {
-   card.classList.add('match');
- });
-}
-
-const resetGuesses = () => {
- firstGuess = '';
- secondGuess = '';
- count = 0;
-
- var selected = document.querySelectorAll('.selected');
- selected.forEach(card => {
-   card.classList.remove('selected');
- });
-};
 
 grid.addEventListener('click', function (event) {
 
@@ -117,4 +100,113 @@ grid.addEventListener('click', function (event) {
     }
      previousTarget = clicked;
   }
+});
+
+
+ const match = () => {
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.add('match');
+  });
+}
+
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};*/
+
+
+const gameGrid = cardsArray
+    .concat(cardsArray)
+    .sort(() => 0.5 - Math.random());
+
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
+
+const game = document.getElementById('game');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+gameGrid.forEach(item => {
+    const { name, img } = item;
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.name = name;
+
+    const front = document.createElement('div');
+    front.classList.add('front');
+
+    const back = document.createElement('div');
+    back.classList.add('back');
+    back.style.backgroundImage = `url(${img})`;
+
+    grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+});
+
+const match = () => {
+    const selected = document.querySelectorAll('.selected');
+    selected.forEach(card => {
+        card.classList.add('match');
+    });
+};
+
+const resetGuesses = () => {
+    firstGuess = '';
+    secondGuess = '';
+    count = 0;
+    previousTarget = null;
+
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach(card => {
+        card.classList.remove('selected');
+    });
+};
+
+grid.addEventListener('click', event => {
+
+    const clicked = event.target;
+
+    if (
+        clicked.nodeName === 'SECTION' ||
+        clicked === previousTarget ||
+        clicked.parentNode.classList.contains('selected') ||
+        clicked.parentNode.classList.contains('match')
+    ) {
+        return;
+    }
+
+    if (count < 2) {
+        count++;
+        if (count === 1) {
+            firstGuess = clicked.parentNode.dataset.name;
+            console.log(firstGuess);
+            clicked.parentNode.classList.add('selected');
+        } else {
+            secondGuess = clicked.parentNode.dataset.name;
+            console.log(secondGuess);
+            clicked.parentNode.classList.add('selected');
+        }
+
+        if (firstGuess && secondGuess) {
+            if (firstGuess === secondGuess) {
+                setTimeout(match, delay);
+            }
+            setTimeout(resetGuesses, delay);
+        }
+        previousTarget = clicked;
+    }
+
 });
