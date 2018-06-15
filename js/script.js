527fa2e6cf0364f13f0d083f000bc6c4c09bbb6b
Greniert <grenierth@hotmail.fr>
2018-06-15 11:17:00 +0200

    image et js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-var carte = document.getElementsByTagName('td');
-
-
-var carte = document.getElementById("pict");
-pictclick.addEventListener('click', cha);
-let nb = 0;
-
-function cha() {
-    if (nb == 0) {
-        pict.style.backgroundColor = 'red';
-        nb = 1;
-    } else {
-        pict.style.backgroundColor = 'black';
-        nb = 0;
-    }
-
+var Tile = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 70;
 };
+
+Tile.prototype.drawFaceDown = function() {
+    fill(214, 247, 202);
+    strokeWeight(2);
+    rect(this.x, this.y, this.width, this.width, 10);
+    image(getImage("img/chouette.jpg"), this.x, this.y, this.width, this.width);
+};
+
+// Create the array of tiles at appropriate positions
+var tiles = [];
+var NUM_COLS = 5;
+var NUM_ROWS = 4;
+for (var i = 0; i < NUM_COLS; i++) {
+    for (var j = 0; j < NUM_ROWS; j++) {
+        tiles.push(new Tile(i * 78 + 10, j * 78 + 40));
+    }
+}
+
+// Start by drawing them all face down
+for (var i = 0; i < tiles.length; i++) {
+    tiles[i].drawFaceDown();
+}
