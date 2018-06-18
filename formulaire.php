3d246aaf290fbb3250dd2137e7d6b1b520a6c2ac
aportier2018 <anne.portier@wanadoo.fr>
2018-06-18 14:10:28 +0200

    formulaire et css recap formulaire anne

--- formulaire.php
+++ formulaire.php
@@ -3,26 +3,28 @@
 <html lang=fr dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/style.css">
-    <title></title>
+    <link rel="stylesheet" href="css/recapform.css">
+
+    <title>récapitulatif du formulaire</title>
 
   </head>
-  <body>
-    <?php
-       /*if(isset($_POST['envoyer'])) {*/
+  <body class="page">
+    <div class= "recap">
+      <?php
+
             $civilite = $_POST['civilite'];
             $nom = htmlspecialchars($_POST['nom'], ENT_QUOTES);
             $prenom = htmlspecialchars($_POST['prenom'], ENT_QUOTES);
             $email = htmlspecialchars($_POST['email'], ENT_QUOTES);
 
-        /*  }*/
-
 echo "<h1> Votre formulaire:</h1>";
-echo "<p> Merci d'avoir jouer " .$civilite." ".$nom." ".$prenom.".</p>";
-echo "<p> Votre mail est : ".$email.".</p>"
-?>
+echo "<p> Merci " .$civilite." ".$nom." ".$prenom." d'avoir jouer.</p>";
+echo "<p> Votre mail est : ".$email.".</p>";
 
+?>
     <p> Pour modifier votre formulaire, cliquez <a href="formulaire.html">ici</a>.</p>
-    <p> Allez, on y retourne ? <a href="index.html"><button type ="button" class ="button">Rejouer</button></a></p>
+    <p> Allez, on y retourne ?<a href="index.html"><button type ="button" class ="bouton">Rejouer</button></a></p>
+  </div>
+
   </body>
 </html>
